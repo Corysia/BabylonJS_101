d18361969294f794810432a35fd6cb4cc731ba66
Corysia Taware <corysia.taware@gmail.com>
2022-05-05 13:54:45 -0700

    02. Standard Materials in BabylonJS

--- b101/src/BabylonExamples/StandardMaterials.ts
+++ b101/src/BabylonExamples/StandardMaterials.ts
@@ -1,10 +1,12 @@
 import {
-    Scene, 
-    Engine, 
+    Scene,
+    Engine,
     FreeCamera,
     Vector3,
     HemisphericLight,
     MeshBuilder,
+    StandardMaterial,
+    Texture,
 } from '@babylonjs/core';
 
 export class StandardMaterials {
@@ -27,16 +29,71 @@ export class StandardMaterials {
         const scene = new Scene(this.engine);
         const camera = new FreeCamera("camera", new Vector3(0, 1, -5), this.scene);
         camera.attachControl();
+        camera.speed = 0.25;
 
         const hemiLight = new HemisphericLight("hemiLight", new Vector3(0, 1, 0), this.scene);
-        hemiLight.intensity = 0.5;
+        hemiLight.intensity = 1.0;
 
-        const ground = MeshBuilder.CreateGround("ground", {width: 10, height: 10}, this.scene);
+        const ground = MeshBuilder.CreateGround("ground", { width: 10, height: 10 }, this.scene);
 
-        const ball = MeshBuilder.CreateSphere("ball", {diameter: 1}, this.scene);
+        const ball = MeshBuilder.CreateSphere("ball", { diameter: 1 }, this.scene);
         ball.position.y = 1;
 
+        ground.material = this.CreateGroundMaterial();
+        ball.material = this.CreateBallMaterial();
         return scene;
     }
 
+    CreateGroundMaterial(): StandardMaterial {
+        const groundMat = new StandardMaterial("groundMat", this.scene);
+        const uvScale = 4;
+        const texArray: Texture[] = [];
+        const normalTex = new Texture("./images/stone/cobblestone_05_nor_gl_1k.jpg", this.scene);
+        const aoTex = new Texture("./images/stone/cobblestone_05_ao_1k.jpg", this.scene);
+        const specTex = new Texture("./images/stone/cobblestone_05_spec_1k.jpg", this.scene);
+        const diffuseTex = new Texture("./images/stone/cobblestone_05_diff_1k.jpg", this.scene);
+
+        groundMat.diffuseTexture = diffuseTex;
+        groundMat.bumpTexture = normalTex;
+        groundMat.ambientTexture = aoTex;
+        groundMat.specularTexture = specTex;
+        texArray.push(diffuseTex);
+        texArray.push(normalTex);
+        texArray.push(aoTex);
+        texArray.push(specTex);
+
+        texArray.forEach(tex => {
+                tex.uScale = uvScale;
+                tex.vScale = uvScale;
+        });
+        return groundMat;
+    }
+
+    CreateBallMaterial(): StandardMaterial {
+        const ballMat = new StandardMaterial("ballMat", this.scene);
+        const uvScale = 1;
+        const texArray: Texture[] = [];
+        const normalTex = new Texture("./images/metal/metal_plate_nor_gl_1k.jpg", this.scene);
+        const aoTex = new Texture("./images/metal/metal_plate_ao_1k.jpg", this.scene);
+        const specTex = new Texture("./images/metal/metal_plate_spec_1k.jpg", this.scene);
+        const diffuseTex = new Texture("./images/metal/metal_plate_diff_1k.jpg", this.scene);
+
+        ballMat.diffuseTexture = diffuseTex;
+        ballMat.bumpTexture = normalTex;
+        ballMat.invertNormalMapX = true;
+        ballMat.invertNormalMapY = true;
+        ballMat.ambientTexture = aoTex;
+        ballMat.specularTexture = specTex;
+        ballMat.specularPower = 1;
+        texArray.push(diffuseTex);
+        texArray.push(normalTex);
+        texArray.push(aoTex);
+        texArray.push(specTex);
+
+        texArray.forEach(tex => {
+                tex.uScale = uvScale;
+                tex.vScale = uvScale;
+        });
+        return ballMat;
+    }
 }
